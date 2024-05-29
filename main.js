77f8af8fd0f47785635275979b76765a490a191f
Alexander <a.carlssons98@gmail.com>
2024-05-29 20:32:47 +0200

    started on the javascript for the project

---
+++ main.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function newBook() {
+    let book = prompt("Give me a book");
+    
+}
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
