e68662b69076e253c901bd4e40f259253e48ac64
Alexander <a.carlssons98@gmail.com>
2024-05-29 20:56:29 +0200

    added a new function addBookToLibrary and made it push the new book made to the myLibrary array when pressing submit button

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 const myLibrary = [];
 
-function newBook() {
-    
-}
-
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
+
+let dialog = document.querySelector("#dialog");
+
+let newBookBtn = document.querySelector("#new-book-btn");
+newBookBtn.addEventListener("click", function() {
+    dialog.show();
+
+})
+
+function addBookToLibrary() {
+    let title = document.querySelector("#title").value
+    let author = document.querySelector("#author").value
+    let pages = document.querySelector("#pages").value
+    let read = document.querySelector("#read").value
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    console.log(newBook);
+    console.log(myLibrary);
+
+    
+  }
+
+  let submitBtn = document.querySelector("#submit");
+  submitBtn.addEventListener("click", function () {
+
+    addBookToLibrary();
+
+  })
+
